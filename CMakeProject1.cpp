a68b61aa24171466437d96044635e7cc705d1bd9
Joshua Froggatt <jfrogg.au@gmail.com>
2024-03-05 19:54:04 +1030

    modified:   CMakeProject1.cpp Finished Activity 1

--- CMakeProject1.cpp
+++ CMakeProject1.cpp
@@ -10,25 +10,37 @@ void function2(int*);
 int main() {
 	int number = 10;
 	/* C1. display the contents of number to the screen */
-	std::cout << number << std::endl;
+	std::cout << "'number' equals to " << number << std::endl;
 	/* C2. display the address of variable number to the screen */
+	std::cout << "The address of 'number' equals to " << &number << std::endl;
 	/* C3. call function1 */
+	function1(number);
 	/* C4. display the contents of number to the screen */
+	std::cout << "after calling 'function1', 'number' is now equal to " << number << std::endl;
 	/* C5. call function2 */
+	function2(&number);
 	/* C6. display the contents of number to the screen */
+	std::cout << "after calling 'function2', 'number' is now equal to " << number << std::endl;
 	return 0;
 }
 void function1(int number)
 {
 	/* C7. increment number by one */
+	number = number + 1;
 	/* C8. display the contents of number to the screen (should be 11) */
+	std::cout << "in 'function1', 'number' is now equal to " << number << std::endl;
 	/* C9. display the address of number to the screen */
+	std::cout << "in 'function1', the address of 'number' is " << &number << std::endl;
 }
 void function2(int* numberPtr)
 {
 	/* C10. display the address of number to the screen (should be the same
 	address as you displayed first in main) */
+	std::cout << "in 'function2', the value of 'numberPtf', the pointer of 'number' is " << numberPtr << std::endl;
 	/* C11. display the value that the pointer numberPtr points to */
+	std::cout << "in 'function2', the value of 'number' is " << *numberPtr << std::endl;
 	/* C12. increment the value stored in number */
+	*numberPtr = *numberPtr + 1;
 	/* C13. display the contents of number to the screen */
+	std::cout << "in 'function2', the value of 'number' is now " << *numberPtr << std::endl;
 }
